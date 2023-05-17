ad28226029ac9d3ab7d1e0ce67e6307f6869864d
Prince Narteh <prinarteh@gmail.com>
2023-05-17 13:57:05 +0000

    fix search bar

--- components/layout/SearchBar.tsx
+++ components/layout/SearchBar.tsx
@@ -5,10 +5,13 @@ import { BiSearch } from "react-icons/bi";
 import { BsHeart } from "react-icons/bs";
 import { CiUser } from "react-icons/ci";
 import { TiShoppingCart } from "react-icons/ti";
+import { IoMdHome } from "react-icons/io";
 
 export const SearchBar = () => {
   return (
-    <div className={`fixed z-50 h-16 w-full cursor-pointer p-5 shadow-lg`}>
+    <div
+      className={`fixed z-50 w-full cursor-pointer px-5 py-3 gap-2 shadow-lg`}
+    >
       <Image
         src="/images/search-bg.jpg"
         className="absolute object-cover duration-500"
@@ -16,30 +19,38 @@ export const SearchBar = () => {
         alt=""
       />
       <div className="absolute inset-0 h-full w-full bg-black opacity-60"></div>
-      <div className="relative z-10 flex h-full w-full items-center justify-between">
-        <div className="bg-white p-1 rounded">
-          <Image src={"/images/logo.png"} width={50} height={100} alt="logo" />
-        </div>
-        <div className="relative flex w-full bg-white max-w-2xl items-center rounded-full border-2 px-5">
-          <input
-            type="text"
-            className="flex-1 p-2 outline-none"
-            placeholder="Search for product..."
-          />
-          <BiSearch className="w-10 shrink-0 text-3xl text-gray-500" />
+      <div className="relative z-10 flex flex-col gap-3 h-full w-full items-center justify-between">
+        <div className="">
+          {/* Logo */}
+          <div className="bg-white p-1 rounded">
+            <Image src={"/images/logo.png"} width={20} height={50} alt="logo" />
+          </div>
         </div>
 
-        <div className="flex items-center space-x-5 text-pink-500">
-          <Link href={"/"}>Home</Link>
-          <CiUser className="cursor-pointer text-3xl " />
-          <BsHeart className="cursor-pointer text-2xl " />
-          <Link href={`/cart`} className="relative">
-            <TiShoppingCart className="cursor-pointer text-3xl " />
-            <div className="absolute -right-1.5 -top-1.5 flex h-5  w-5 items-center justify-center rounded-full bg-[red]">
-              <span className="text-[10px] text-white">20</span>
-            </div>
-          </Link>
+        <div className="flex justify-between w-full">
+          {/* Search Bar */}
+          <div className="relative flex  bg-white max-w-2xl items-center rounded-full border-2 px-5">
+            <input
+              type="text"
+              className="w-32 outline-none"
+              placeholder="Search for product..."
+            />
+            <BiSearch className="w-10 shrink-0 text-xl text-gray-500" />
+          </div>
+
+          <div className="flex items-center justify-end space-x-1 text-pink-500">
+            {/* <IoMdHome className="text-xl" /> */}
+            <CiUser className="cursor-pointer text-2xl " />
+            {/* <BsHeart className="cursor-pointer text-2xl " /> */}
+            <Link href={`/cart`} className="relative">
+              <TiShoppingCart className="cursor-pointer text-2xl " />
+              <div className="absolute -right-1.5 -top-1.5 flex h-5  w-5 items-center justify-center rounded-full bg-[red]">
+                <span className="text-[10px] text-white">20</span>
+              </div>
+            </Link>
+          </div>
         </div>
+        {/* Menu */}
       </div>
     </div>
   );
