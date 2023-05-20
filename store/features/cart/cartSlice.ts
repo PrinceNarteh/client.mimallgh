1eeddb2f14608135423a1932e4c7847097508b45
Prince Narteh <prinarteh@gmail.com>
2023-05-20 04:23:28 +0000

    add persistance to state mgt

---
+++ store/features/cart/cartSlice.ts
@@ -0,0 +1,85 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type CartItem = {
+  productName: string;
+  quantity: number;
+  price: number;
+  productId: string;
+  shopId: string;
+};
+
+type Cart = {
+  totalAmount: number;
+  totalCount: number;
+  items: CartItem[];
+};
+
+const initialState: Cart = {
+  totalAmount: 0,
+  totalCount: 0,
+  items: [],
+};
+
+export const CartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    getCartTotal: (state) => {
+      let { totalAmount, totalCount } = state.items.reduce(
+        (cartTotal, cartItem) => {
+          const { price, quantity } = cartItem;
+          const itemTotal = price * quantity;
+
+          cartTotal.totalAmount += itemTotal;
+          cartTotal.totalCount += quantity;
+          return cartTotal;
+        },
+        {
+          totalAmount: 0,
+          totalCount: 0,
+        }
+      );
+      state.totalAmount = parseInt(totalAmount.toFixed(2));
+      state.totalCount = totalCount;
+    },
+    remove: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.productId !== action.payload
+      );
+    },
+    increase: (state, action) => {
+      state.items = state.items.map((item) => {
+        if (item.productId === action.payload) {
+          return { ...item, amount: item.quantity + 1 };
+        }
+        return item;
+      });
+    },
+    decrease: (state, action) => {
+      state.items = state.items
+        .map((item) => {
+          if (item.productId === action.payload) {
+            return { ...item, amount: item.quantity - 1 };
+          }
+          return item;
+        })
+        .filter((item) => item.quantity !== 0);
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+    getCartItems: (state) => {
+      state.items = state.items;
+    },
+  },
+});
+
+export default CartSlice.reducer;
+export const {
+  getCartTotal,
+  remove,
+  increase,
+  decrease,
+  clearCart,
+  getCartItems,
+} = CartSlice.actions;
--- store/store.ts
+++ store/store.ts
@@ -1,11 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { ProductSlice } from "./features/products/productSlice";
 import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "redux";
+import { CartSlice } from "./features/cart/cartSlice";
 
-export const store = configureStore({
-  reducer: {
-    products: ProductSlice.reducer,
-  },
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  product: ProductSlice.reducer,
+  cart: CartSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
 });
 
 export const useAppDispatch: () => typeof store.dispatch = useDispatch;
