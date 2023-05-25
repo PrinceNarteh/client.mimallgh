528914b839c826704e7d0cb5386a07496e9e9846
Prince Narteh <prinarteh@gmail.com>
2023-05-25 02:48:47 +0000

    refactor video player component

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
-
-const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
+import React, { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
 
 const VideoPlayer = ({ idx }: { idx: number }) => {
   const [showControls, setShowControls] = useState(false);
   const [activePlayer, setActivePlayer] = useState<number | null>(null);
+  const [hasWindow, setHasWindow] = useState(false);
 
   const handleMouseEnter = (currentItem: number) => {
     setShowControls(true);
@@ -17,18 +16,26 @@ const VideoPlayer = ({ idx }: { idx: number }) => {
     setActivePlayer(null);
   };
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
+
   return (
     <div
       onMouseEnter={() => handleMouseEnter(idx)}
       onMouseLeave={() => handleMouseLeave()}
     >
-      <ReactPlayer
-        url={"/videos/sea-shore.mp4"}
-        width={"100%"}
-        height={"100%"}
-        muted
-        controls={showControls && activePlayer === idx}
-      />
+      {hasWindow && (
+        <ReactPlayer
+          url={"/videos/sea-shore.mp4"}
+          width={"100%"}
+          height={"100%"}
+          muted
+          controls={showControls && activePlayer === idx}
+        />
+      )}
     </div>
   );
 };
