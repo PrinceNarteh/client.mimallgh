f798e8c1e90b00c465c0ef5bce89c418310d6324
Prince Narteh <prinarteh@gmail.com>
2023-05-28 05:00:40 +0000

    add addressing in confirm order page

--- pages/checkout/confirm-order.tsx
+++ pages/checkout/confirm-order.tsx
@@ -1,9 +1,11 @@
 import { OrderSummary } from "@/components";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import type { FormEvent } from "react";
 
 const Checkout = () => {
   const router = useRouter();
+  const { data: session } = useSession();
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,52 +22,86 @@ const Checkout = () => {
           <div className="col-span-12 lg:col-span-8">
             <OrderSummary />
           </div>
-          <div className="col-span-12 w-full lg:col-span-4">
-            <h4 className="sh-underline relative md:text-2xl">Address</h4>
-            <form onSubmit={handleSubmit} className="mt-5 space-y-5 w-full">
-              <div className="">
+          <div className="col-span-12 w-full lg:col-span-4 space-y-5">
+            <div>
+              <h4 className="sh-underline relative md:text-2xl mb-3">
+                Address
+              </h4>
+              <div>
+                <input type="radio" name="address" id="oldAddress" checked />
                 <label
-                  htmlFor="base-input"
-                  className="block mb-1 text-md font-medium text-gray-900"
+                  htmlFor="oldAddress"
+                  className="ml-2 inline-block cursor-pointer"
                 >
-                  Full Name
+                  Use Existing Address
                 </label>
-                <input
-                  type="text"
-                  id="base-input"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full"
-                />
+                <p className="text-sm pl-5 font-semibold">
+                  {session?.user?.firstName} {session?.user?.lastName} -{" "}
+                  {session?.user?.email}
+                </p>
               </div>
-              <div className="w-full">
-                <label
-                  htmlFor="phone-number"
-                  className="block mb-1 text-md font-medium text-gray-900"
-                >
-                  Phone Number
-                </label>
-                <input
-                  type="text"
-                  id="phone-number"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                />
-              </div>
-              <div className="w-full">
-                <label
-                  htmlFor="alternate-phone-number"
-                  className="block mb-1 text-md font-medium text-gray-900"
-                >
-                  Alternate Phone Number
-                </label>
-                <input
-                  type="text"
-                  id="alternate-phone-number"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                />
-              </div>
-              <button className="block w-full rounded-md bg-pink-500 py-3 text-center font-bold text-white">
-                Confirm Order
-              </button>
-            </form>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="address"
+                id="newAddress"
+                className="peer"
+              />
+              <label
+                htmlFor="newAddress"
+                className="ml-2 inline-block cursor-pointer"
+              >
+                Add New Address
+              </label>
+              <form
+                onSubmit={handleSubmit}
+                className="hidden mt-5 space-y-3 w-full peer-checked:block"
+              >
+                <div className="">
+                  <label
+                    htmlFor="base-input"
+                    className="block mb-1 text-md font-medium text-gray-900"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    id="base-input"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2 outline-none w-full"
+                  />
+                </div>
+                <div className="w-full">
+                  <label
+                    htmlFor="phone-number"
+                    className="block mb-1 text-md font-medium text-gray-900"
+                  >
+                    Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    id="phone-number"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 outline-none"
+                  />
+                </div>
+                <div className="w-full">
+                  <label
+                    htmlFor="alternate-phone-number"
+                    className="block mb-1 text-md font-medium text-gray-900"
+                  >
+                    Alternate Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    id="alternate-phone-number"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 outline-none"
+                  />
+                </div>
+              </form>
+            </div>
+            <button className="block w-full rounded-md bg-pink-500 py-3 text-center font-bold text-white">
+              Confirm Order
+            </button>
           </div>
         </div>
       </div>
