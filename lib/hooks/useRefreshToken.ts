3480bdc8349ccec92e69039224ea521e021367f1
Prince Narteh <prinarteh@gmail.com>
2023-06-24 11:33:09 +0000

    implement refresh token with fetch api

--- lib/hooks/useRefreshToken.ts
+++ lib/hooks/useRefreshToken.ts
@@ -1,16 +1,18 @@
-import axios from "../axios";
 import { signIn, useSession } from "next-auth/react";
 
 export const useRefreshToken = () => {
   const { data: session } = useSession();
 
   const refreshToken = async () => {
-    const res = await axios.post("/shop-auth/refresh", {
-      refresh: session?.user?.refreshToken,
+    const res = await fetch("/auth/refresh", {
+      method: "POST",
+      body: JSON.stringify({ refresh: session?.user?.refreshToken }),
     });
 
+    const token = await res.json();
+
     if (session && session.user) {
-      session.user.accessToken = res.data.accessToken;
+      session.user.accessToken = token.data.accessToken;
     } else {
       signIn();
     }
