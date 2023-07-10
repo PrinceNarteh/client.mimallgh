65133ed73ea8c8a56328090d5c6e95220bc081b8
Prince Narteh <prinarteh@gmail.com>
2023-07-10 02:25:48 +0000

    add section two

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ const Home = () => {
           </ul>
         </div>
       </nav>
+      {/* SECTION ONE */}
       <section className="p-5 min-h-[calc(100vh_-_68px)] max-w-7xl mx-auto overflow-hidden">
         <h2 className="text-center text-orange-500 text-3xl font-bold">
           WHAT WE DO
@@ -77,6 +78,65 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      {/* SECTION TWO */}
+      <div className="bg-gray-100">
+        <section className="flex items-center p-5 min-h-screen max-w-7xl mx-auto overflow-hidden">
+          <div>
+            <h3 className="text-navy-blue text-3xl text-center mb-10">
+              MiMall eEasy-coms
+            </h3>
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-5">
+              <div className="max-h-fit hidden bg-red-500 col-span-4 rounded-md overflow-hidden lg:block">
+                <img
+                  src="/images/section-two.jpeg"
+                  alt=""
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="col-span-12 md:col-span-8">
+                <div className="text-justify bg-navy-blue text-white p-5 md:text-xl rounded">
+                  b. We connect you to Top sellers in Fashion, Techs, Pharmacy,
+                  etc. in and outside campus.
+                </div>
+
+                <div className="grid grid-auto-fit gap-3 mt-5">
+                  <img
+                    src="/images/sec-one-img-one.jpg"
+                    alt=""
+                    className="rounded object-cover"
+                  />
+                  <img
+                    src="/images/sec-one-img-two.jpg"
+                    alt=""
+                    className="rounded object-cover"
+                  />
+                  <img
+                    src="/images/sec-one-img-three.jpeg"
+                    alt=""
+                    className="hidden rounded object-cover md:block"
+                  />
+                  <img
+                    src="/images/sec-one-img-four.jpg"
+                    alt=""
+                    className="hidden rounded object-cover md:block"
+                  />
+                  {/* <img
+                  src="/images/sec-one-img-five.jpg"
+                  alt=""
+                  className="hidden rounded object-cover lg:block"
+                />
+                <img
+                  src="/images/sec-one-img-six.jpg"
+                  alt=""
+                  className="hidden rounded object-cover lg:block"
+                /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
